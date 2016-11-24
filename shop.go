5a5554dd78878236adab652f72619e995aabacb0
felipe <luizfelipetx@gmail.com>
2016-11-24 18:37:11 -0200

    adding ignore itens

--- shop.go
+++ shop.go
@@ -19,15 +19,15 @@ func setupCatelog() list.List{
 	alist := list.List{}
 
 	for e := 0 ; e <10 ; e++{
-		product := &Product{"Cabify T-Shirt",20,"TSHIRT"}
+		product := &Product{"T-Shirt",20,"TSHIRT"}
 		alist.PushBack(product)
 	}
 	for e := 10 ; e <20 ; e++{
-		product := &Product{"Cabify Voucher",5,"VOUCHER"}
+		product := &Product{"Voucher",5,"VOUCHER"}
 		alist.PushBack(product)
 	}
 	for e := 20 ; e <30 ; e++{
-		product := &Product{"Cabify Mug",8,"MUG"}
+		product := &Product{"Mug",8,"MUG"}
 		alist.PushBack(product)
 	}
 	return alist;
